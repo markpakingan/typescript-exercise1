ba1e0feb4b6a8db497e89dbc86b3bebb8730ddf3
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-01-28 19:37:48 -0800

    starter codes

---
+++ exercise.ts
@@ -0,0 +1,21 @@
+// Create a function that takes an array of numbers as 
+// input and returns the sum of all the even numbers in the array.
+
+
+const sumOfEvenNums = (numbers : number[]) : number => {
+
+    if (numbers.length === undefined) {
+        return 0;
+    }
+
+    let sum = 0; 
+
+    for(let num of numbers){
+        if(num % 2 === 0){
+            sum += num;
+        }
+    }
+
+    return sum;
+
+}
